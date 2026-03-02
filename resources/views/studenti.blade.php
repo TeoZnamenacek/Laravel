e99c5e3f3f4c0cf04aca731df74137f3f7c87c93
Teo Znamenaček <teo.znamenacek@ad.tsd.hr>
2026-03-02 11:04:14 +0100

    završeni vođeni zadatci

--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -12,17 +12,7 @@
     <h1 class="mb-4">🎓 Studenti</h1>
     <div class="row">
         @foreach($studenti as $student)
-            <div class="col-md-3 mb-3">
-                <div class="card border-{{ $student['polozio'] ? 'success' : 'danger' }}">
-                    <div class="card-header bg-{{ $student['polozio'] ? 'success' : 'danger' }} text-white">
-                        @if($student['polozio']) ✅ Položio @else ❌ Nije položio @endif
-                    </div>
-                    <div class="card-body">
-                        <h6>{{ $student['ime'] }}</h6>
-                        <small class="text-muted">{{ $student['smjer'] }}</small>
-                    </div>
-                </div>
-            </div>
+            @include('partials._student-kartica', ['student' => $student])
         @endforeach
     </div>
 @endsection
